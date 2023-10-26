68e80ae14e3089dfad1f04f6908936e61c4d301a
marioraldaz <130656068+marioraldaz@users.noreply.github.com>
2023-10-26 13:39:44 +0200

    Ya se pintan cartas

--- ej_arrays/funciones.php
+++ ej_arrays/funciones.php
@@ -7,19 +7,28 @@
 </head>
 <body>
     <?php
-    $oros=[];
-    $copas=[];
-    $bastos=[];
-    $espadas=[];
-    for ($i = 1 ; $i <= 12 ; $i++ ) {
-        array_push($oros, $i."oros");
-        array_push($copas, $i."copas");
-        array_push($bastos, $i."bastos");
-        array_push($espadas, $i."espadas");
-        print "<p>".$oros[$i-1]."</p>";
+
+    function crearBaraja() {
+        $oros=[];
+        $copas=[];
+        $bastos=[];
+        $espadas=[];
+        for ($i = 1 ; $i <= 12 ; $i++ ) {
+            array_push($oros, $i."oros");
+            array_push($copas, $i."copas");
+            array_push($bastos, $i."bastos");
+            array_push($espadas, $i."espadas");
+        }
+        return $GLOBALS['baraja']=[$oros, $copas, $bastos, $espadas];
     }
-    $baraja=[$oros, $copas, $bastos, $espadas];
+
+    $baraja = crearBaraja();
+
+    function pintarCartas($baraja, $palo, $carta) {
+        print "<img src='img/".$baraja[array_search($palo, $baraja)][array_search($carta, $palo)].".jpg' />";
+    }
     
+    echo pintarCartas($baraja, $baraja[1], $baraja[1][3]);
     ?>
 </body>
 </html>
