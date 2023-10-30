2b37ed002f6cb40df6908f5fa6342af5b449687d
marioraldaz <130656068+marioraldaz@users.noreply.github.com>
2023-10-30 11:12:31 +0100

    clase-30/10

--- ej_arrays/funciones.php
+++ ej_arrays/funciones.php
@@ -28,20 +28,69 @@
         print "<img src='img/".$baraja[array_search($palo, $baraja)][array_search($carta, $palo)].".jpg' />";
     }
     
-    function seleccionaCartas($baraja, $numCartas, $random) {
+    function seleccionaCartas($numCartas, $random, $palo) {
+        $arrayCartas=[];
+        $baraja = crearBaraja();
         if ($random) {
-            for ($i = 0 ; $i<$numCartas ; $i++) {
-                $paloRandom=rand(0, 3);
-                $cartaRandom=rand(0,11);
-                pintarCartas($baraja, $baraja[$paloRandom], $baraja[$paloRandom][$cartaRandom]);
-            }
+                for ($i = 0 ; $i<$numCartas ; $i++) {
+                    $repetido=false;
+                    while ($repetido === false) {
+                        $paloRandom=rand(0, 3);
+                        $cartaRandom=rand(0,11);
+                        if (!in_array($baraja[$paloRandom][$cartaRandom], $arrayCartas)) {
+                            array_push($arrayCartas, $baraja[$paloRandom][$cartaRandom]);
+                            pintarCartas($baraja, $baraja[$paloRandom], $baraja[$paloRandom][$cartaRandom]);
+                            $repetido = true;
+                        }
+                        
+                    }
+                    
+                    
+                }
         }
         else {
-
+            for ($i = 0 ; $i<$numCartas ; $i++) {
+                $repetido=false;
+                while ($repetido === false) {
+                    $j = rand(0, sizeof($palo)-1);
+                    if ($palo[$j] === "oros") {
+                        $cartaRandom=rand(0,11);
+                        if (!in_array($baraja[0][$cartaRandom], $arrayCartas)) {
+                            array_push($arrayCartas, $baraja[0][$cartaRandom]);
+                            pintarCartas($baraja, $baraja[0], $baraja[0][$cartaRandom]);
+                            $repetido = true;
+                        }
+                    }
+                    elseif ($palo[$j] === "copas") {
+                        $cartaRandom=rand(0,11);
+                        if (!in_array($baraja[1][$cartaRandom], $arrayCartas)) {
+                            array_push($arrayCartas, $baraja[1][$cartaRandom]);
+                            pintarCartas($baraja, $baraja[1], $baraja[1][$cartaRandom]);
+                            $repetido = true;
+                        }
+                    }
+                    elseif ($palo[$j] === "bastos") {
+                        $cartaRandom=rand(0,11);
+                        if (!in_array($baraja[2][$cartaRandom], $arrayCartas)) {
+                            array_push($arrayCartas, $baraja[2][$cartaRandom]);
+                            pintarCartas($baraja, $baraja[2], $baraja[2][$cartaRandom]);
+                            $repetido = true;
+                        }
+                    }
+                    elseif ($palo[$j] === "espadas") {
+                        $cartaRandom=rand(0,11);
+                        if (!in_array($baraja[3][$cartaRandom], $arrayCartas)) {
+                            array_push($arrayCartas, $baraja[3][$cartaRandom]);
+                            pintarCartas($baraja, $baraja[3], $baraja[3][$cartaRandom]);
+                            $repetido = true;
+                        }
+                    }
+                    
+                }
+            }
         }
     }
 
-    seleccionaCartas($baraja, 5, true);
     ?>
 </body>
 </html>
