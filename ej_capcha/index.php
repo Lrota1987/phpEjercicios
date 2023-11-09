3963d968435c30903097dc5f11de03cd02f2ede3
marioraldaz <130656068+marioraldaz@users.noreply.github.com>
2023-11-09 10:50:09 +0100

    clase 09/11/23

--- ej_capcha/index.php
+++ ej_capcha/index.php
@@ -4,42 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <?php
-    
-    
-    $path="./imagenes/imagen.png";
-    $palabra = [];
-    $image=imagecreate(200,100);
-    $fuente="./fuente\RubikMaze-Regular.ttf";
-    for ($i=0 ; $i<imagesx($image) ; $i++) {
-        $color=imagecolorallocate($image,rand(0, 255), rand(0, 255), rand(0, 255));
-        for ($j=0 ; $j<imagesy($image) ; $j++) {
-            
-            imagesetpixel($image, $i, $j, $color);
-        }
-    }
-    $permited_charts='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $arrayLetras=array();
-    
-    for ($i=0 ; $i<7 ; $i++) {
-        $palabraRandom=substr(str_shuffle($permited_charts),0,1);
-        array_push($arrayLetras, $palabraRandom);
-    }
-    for ($i=0 ; $i<sizeof($arrayLetras) ; $i++) {
-        $colorfuente=imagecolorallocate($image,rand(0, 255), rand(0, 255), rand(0, 255));
-        imagefttext($image, 30, random_int(0, 180), 20*($i+1), 50,$colorfuente,$fuente, $arrayLetras[$i] );
-    }
-    var_dump($arrayLetras);
-    $im = imagepng($image, $path);
-    print "<img src=$path />";
-    
-
+       require("imageCreator.php");
+        $arrayGlobal=crearArray();
+        $arrayLetras=$arrayGlobal[0];
+        $img=$arrayGlobal[1];
+        setcookie("cadena", implode("",$arrayLetras));
+        echo <<< EOT
+            <div class="formulario">
+                <form action="./result.php" method="post">
+                    <img src='data:image/png;base64,$img' /></br>
+                    </p>
+                    <label>Introduzca el capcha: </label>
+                    <input type="text" name="letrasCapcha"/></br>
+                    <input type="submit" class="submit" value="No soy un robot"/>
+                </form>
+            </div> 
+        EOT;
     ?>
+   
 </body>
-
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Rubik+Maze&display=swap');
-</style>
 </html>
