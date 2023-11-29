7132e0071aec0483ac8813d3027fefbe5d0f94b9
marioraldaz <130656068+marioraldaz@users.noreply.github.com>
2023-11-29 09:33:12 +0100

    clase 29/11/23

--- ej_ahorcado/ahorcado.php
+++ ej_ahorcado/ahorcado.php
@@ -10,6 +10,17 @@
     <div class="header">
         <?php
         session_start();
+
+        if (isset($_POST['reintentar'])) {
+            $_POST['nombre'] = $_SESSION['nombre'];
+            session_destroy();
+            session_start();
+        }
+
+        if (isset($_POST['salir'])) {
+            session_destroy();
+            header("Location:./index.html");
+        }
         
         if (isset($_POST['nombre'])) {
             $_SESSION['letra']="";
@@ -20,6 +31,7 @@
             $_SESSION['palabra']=strtoupper($arrayPalabras[rand(0, sizeof($arrayPalabras)-1)]);
             $_SESSION['contError']=-1;
             $_SESSION['contAciertos']=0;
+            $_SESSION['arrayLetrasAux']=array();
         }
         print "<h1>BIENVENIDO ".$_SESSION['nombre']."</h1>";
         ?>
@@ -36,6 +48,9 @@
             }
             print "<div class='palabra'>";
                 for ($i=0 ; $i<strlen($_SESSION['palabra']) ; $i++) {
+                    if ($_SESSION['letra'] === $_SESSION['palabra'][$i] && !in_array($_SESSION['letra'], $_SESSION['arrayLetrasAux'])) {
+                        $_SESSION['contAciertos']++;
+                    }
                     if ($_SESSION['letra'] === $_SESSION['palabra'][$i] && !in_array($_SESSION['palabra'][$i], $_SESSION['arrayLetrasUs'])) {
                         print $_SESSION['palabra'][$i]." ";
                         array_push($_SESSION['arrayLetrasUs'],$_SESSION['palabra'][$i]);
@@ -43,26 +58,38 @@
                     }
                     elseif (in_array($_SESSION['palabra'][$i], $_SESSION['arrayLetrasUs'])) {
                         print $_SESSION['palabra'][$i]." ";
+                        
                     }
                     else {
                         print "_ ";
                     }
                 }
+            array_push($_SESSION['arrayLetrasAux'],$_SESSION['letra']);
             print "</div>";
-
+            print "<p>".var_dump($_SESSION['contAciertos'])."</p>";
             print "<div class='errores'>";
                 if ($fallo===true) {
                     $_SESSION['contError']++;
                 }
-                else {
-                    $_SESSION['contAciertos']++;
-                }
+
                 if ($_SESSION['contError'] === 8) {
                     print "HAS PERDIDO ".$_SESSION['nombre'].", ERES UN MONGOLO";
+                    echo <<< EOT
+                    <form action='' method='POST'>
+                        <input type="submit" name="reintentar" value="Reintentar" class="button reintentar" />
+                        <input type="submit" name="salir" value="Salir" class="button salir" />
+                    </form>
+                    EOT;
                 }
                 else {
                     if ($_SESSION['contAciertos'] === strlen($_SESSION['palabra'])) {
                         print "HAS GANADO ".$_SESSION['nombre'].", ERES UN DIOS CON UN INTELÉCTO OLÍMPICO.";
+                        echo <<< EOT
+                        <form action='' method='POST'>
+                            <input type="submit" name="reintentar" value="Reintentar" class="button reintentar" />
+                            <input type="submit" name="salir" value="Salir" class="button salir" />
+                        </form>
+                        EOT;
                     }
                     else {
                         print "<label>Fallos: </label>";
@@ -78,14 +105,20 @@
 
             print "<div class='teclado'>";
                 print "<form action='' method='POST'>";
-                for ($i=0 ; $i<sizeof($_SESSION['alfabeto']) ; $i++) {
-                    print "<input type='submit' name='letra' value='".$_SESSION['alfabeto'][$i]."' />";
-                    $cont++;
-                    if ($cont === 10) {
-                        $cont=0;
-                        print "</br>";
+                if ($_SESSION['contAciertos'] !== strlen($_SESSION['palabra'])) {
+                    if ($_SESSION['contError'] !== 8) {
+                        for ($i=0 ; $i<sizeof($_SESSION['alfabeto']) ; $i++) {
+                            print "<input type='submit' name='letra' value='".$_SESSION['alfabeto'][$i]."' />";
+                            $cont++;
+                            if ($cont === 10) {
+                                $cont=0;
+                                print "</br>";
+                            }
+                        }
                     }
+
                 }
+
                 print "</form>";
             print "</div>";
         ?>
